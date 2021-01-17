53da01e07d51125759434389494662bf4cacd98a
Scott Jacobsen <wjj170030@utdallas.edu>
2021-01-17 15:20:36 -0600

    Init Commit

---
+++ VolumeMixer/MainWindow.xaml.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace VolumeMixerDesktop {
+    /// <summary>
+    /// 4 columns that specify the idType and match settings for the 4 knobs.
+    /// A multiselect that specifies which unit to recieve commands from.
+    /// </summary
+    public partial class MainWindow : Window {
+        public static HotKey VolumeUpCmd;
+        public static HotKey VolumeDownCmd;
+        public static HotKey ToggleMuteCmd;
+        private static MatchOption FocusedProcess;
+        public MainWindow() {
+            InitializeComponent();
+            WebServer ws = new WebServer();
+            availableListReload(null, null);
+        }
+
+        private void availableListReload(object sender, RoutedEventArgs e) {
+            List<MatchOption> matchItems = new List<MatchOption>();
+            foreach (var process in Process.GetProcesses()) {
+                if (VolumeMixerInterFace.GetApplicationVolume(process.Id) != null) {
+                    matchItems.Add(new MatchOption { Display = process.ProcessName + " - " + process.Id, Process = process });
+                }
+            }
+            if (availableList != null) {
+                matchItems.Sort((a, b) => a.Display.CompareTo(b.Display));
+                availableList.ItemsSource = matchItems;
+            }
+        }
+
+        // FROM https://stackoverflow.com/a/10280800/4352298
+
+        delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+
+        private const uint WINEVENT_OUTOFCONTEXT = 0;
+        private const uint EVENT_SYSTEM_FOREGROUND = 3;
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint processId);
+
+        public void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime) {
+            GetWindowThreadProcessId(hwnd, out uint pid);
+            Process focused = Process.GetProcessById((int)pid);
+            foreach (MatchOption match in availableList.Items) {
+                if (match.Process.ProcessName == focused.ProcessName) {
+                    FocusedProcess = match;
+                    reloadBound();
+                    return;
+                } 
+            }
+            reloadBound();
+            FocusedProcess = null;
+        }
+
+        // ======================================================
+
+        private void IncrementCheckNumeric(object sender, TextCompositionEventArgs e) {
+            e.Handled = !e.Text.Any(x => Char.IsDigit(x) || '.'.Equals(x));
+        }
+
+        private void availableChanged(object sender, SelectionChangedEventArgs e) {
+            reloadBound();
+        }
+
+        // FROM https://stackoverflow.com/questions/2136431/how-do-i-read-custom-keyboard-shortcut-from-user-in-wpf#
+        private void ShortcutTextBox_PreviewKeyDown(object sender, KeyEventArgs e) {
+            // The text box grabs all input.
+            e.Handled = true;
+
+            // Fetch the actual shortcut key.
+            Key key = (e.Key == Key.System ? e.SystemKey : e.Key);
+
+            // Ignore modifier keys.
+            if (key == Key.LeftShift || key == Key.RightShift
+                || key == Key.LeftCtrl || key == Key.RightCtrl
+                || key == Key.LeftAlt || key == Key.RightAlt
+                || key == Key.LWin || key == Key.RWin) {
+                return;
+            }
+
+            // Build the shortcut key name.
+            StringBuilder shortcutText = new StringBuilder();
+            KeyModifier modifiers = 0;
+            if ((Keyboard.Modifiers & ModifierKeys.Control) != 0) {
+                shortcutText.Append("Ctrl+");
+                modifiers |= KeyModifier.Ctrl;
+            }
+            if ((Keyboard.Modifiers & ModifierKeys.Shift) != 0) {
+                shortcutText.Append("Shift+");
+                modifiers |= KeyModifier.Shift;
+            }
+            if ((Keyboard.Modifiers & ModifierKeys.Alt) != 0) {
+                shortcutText.Append("Alt+");
+                modifiers |= KeyModifier.Alt;
+            }
+            if ((Keyboard.Modifiers & ModifierKeys.Windows) != 0) {
+                shortcutText.Append("Win+");
+                modifiers |= KeyModifier.Win;
+            }
+            shortcutText.Append(key.ToString());
+
+            // Update the text box.
+            TextBox _ShortcutTextBox = (TextBox)sender;
+
+            if (_ShortcutTextBox.Name == "volUpInput") {
+                if (VolumeUpCmd != null) {
+                    VolumeUpCmd.Unregister();
+                }
+                VolumeUpCmd = new HotKey(key, modifiers, VolumeUp);
+            } else if (_ShortcutTextBox.Name == "volDownInput") {
+                if (VolumeDownCmd != null) {
+                    VolumeDownCmd.Unregister();
+                }
+                VolumeDownCmd = new HotKey(key, modifiers, VolumeDown);
+            } else if (_ShortcutTextBox.Name == "toggleMuteInput") {
+                if (ToggleMuteCmd != null) {
+                    ToggleMuteCmd.Unregister();
+                }
+                ToggleMuteCmd = new HotKey(key, modifiers, ToggleMute);
+            }
+
+            _ShortcutTextBox.Text = shortcutText.ToString();
+        }
+
+        private List<MatchOption> reloadBound() {
+            List<MatchOption> bound = new List<MatchOption>();
+            foreach (MatchOption match in availableList.SelectedItems) {
+                bound.Add(match);
+            }
+            if (FocusedProcess != null && (bool)focusedCheckBox.IsChecked && !bound.Contains(FocusedProcess)) {
+                bound.Add(FocusedProcess);
+            }
+            boundList.ItemsSource = bound;
+            return bound;
+        }
+
+        private void ChangeVolume(float step) {
+            foreach (MatchOption match in reloadBound()) {
+                VolumeMixerInterFace.IncrementApplicationVolume(match.Process.Id, step);
+            }
+        }
+
+        private void VolumeUp(HotKey hotKey) {
+            float step = float.TryParse(volStepInput.Text, out step) ? step : 5;
+            ChangeVolume(step);
+        }
+
+        private void VolumeDown(HotKey hotKey) {
+            float step = float.TryParse(volStepInput.Text, out step) ? -1 * step : -5;
+            ChangeVolume(step);
+        }
+
+        private void ToggleMute(HotKey hotKey) {
+            foreach (MatchOption match in reloadBound()) {
+                VolumeMixerInterFace.ToggleApplicationMute(match.Process.Id);
+            }
+        }
+    }
+
+    public class MatchOption {
+        public string Display { get; set; }
+
+        public Process Process { get; set; }
+
+        public List<int> pids { get; set; }
+    }
+}
---
+++ VolumeMixer/WebServer.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+// FROM https://www.c-sharpcorner.com/article/creating-your-own-web-server-using-C-Sharp/
+
+public class WebServer {
+    private TcpListener listener;
+    private int port = 5050;
+
+    public WebServer() {
+        try {
+            //start listing on the given port  
+#pragma warning disable CS0618 // Type or member is obsolete
+            listener = new TcpListener(port);
+#pragma warning restore CS0618 // Type or member is obsolete
+            listener.Start();
+            Log("Web Server Running... Press ^C to Stop...");
+            //start the thread which calls the method 'StartListen'  
+            Thread th = new Thread(new ThreadStart(StartListen));
+            th.Start();
+        } catch (Exception e) {
+            Log("An Exception Occurred while Listening :" + e.ToString());
+        }
+    }
+
+    private void Log(string msg, DateTime? date = null) {
+        if (date == null) {
+            date = DateTime.Now;
+        }
+        Console.WriteLine("[" + date + "]: " + msg);
+    }
+
+    public void SendToBrowser(String sData, ref Socket socket) {
+        SendToBrowser(Encoding.ASCII.GetBytes(sData), ref socket);
+    }
+
+    public void SendToBrowser(Byte[] bSendData, ref Socket socket) {
+        int numBytes = 0;
+        try {
+            if (socket.Connected) {
+                if ((numBytes = socket.Send(bSendData, bSendData.Length, 0)) == -1)
+                    Log("Socket Error cannot Send Packet");
+                else {
+                    Log(String.Format("No. of bytes sent: {0}", numBytes));
+                }
+            } else Log("Connection Dropped....");
+        } catch (Exception e) {
+            Log(String.Format("Error Occurred : {0} ", e));
+        }
+    }
+
+    public void SendHeader(string sHttpVersion, string sMIMEHeader, int iTotBytes, string sStatusCode, ref Socket socket) {
+        String sBuffer = "";
+        // if Mime type is not provided set default to text/html  
+        if (sMIMEHeader.Length == 0) {
+            sMIMEHeader = "text/html";// Default Mime Type is text/html  
+        }
+        sBuffer = sBuffer + sHttpVersion + sStatusCode + "\r\n";
+        sBuffer = sBuffer + "Server: cx1193719-b\r\n";
+        sBuffer = sBuffer + "Content-Type: " + sMIMEHeader + "\r\n";
+        sBuffer = sBuffer + "Accept-Ranges: bytes\r\n";
+        sBuffer = sBuffer + "Content-Length: " + iTotBytes + "\r\n\r\n";
+        Byte[] bSendData = Encoding.ASCII.GetBytes(sBuffer);
+        SendToBrowser(bSendData, ref socket);
+        Log("Total Bytes : " + iTotBytes.ToString());
+    }
+
+    public void StartListen() {
+        int iStartPos = 0;
+        String sRequest;
+        while (true) {
+            //Accept a new connection  
+            Socket socket = listener.AcceptSocket();
+            if (socket.Connected) {
+                Log(String.Format("\nClient Connected!!\n==================\nClient IP {0}\n", socket.RemoteEndPoint));
+                //make a byte array and receive data from the client   
+                Byte[] bReceive = new Byte[1024];
+                int i = socket.Receive(bReceive, bReceive.Length, 0);
+                //Convert Byte to String  
+                string sBuffer = Encoding.ASCII.GetString(bReceive);
+                //At present we will only deal with GET type  
+                if (sBuffer.Substring(0, 3) != "GET") {
+                    Log("Only Get Method is supported..");
+                    socket.Close();
+                }
+                // Look for HTTP request  
+                iStartPos = sBuffer.IndexOf("HTTP", 1);
+                // Get the HTTP text and version e.g. it will return "HTTP/1.1"  
+                string sHttpVersion = sBuffer.Substring(iStartPos, 8);
+                // Extract the Requested Type and Requested file/directory  
+                sRequest = sBuffer.Substring(0, iStartPos - 1);
+                //Replace backslash with Forward Slash, if Any  
+                sRequest.Replace("\\", "/");
+                string output = "No options provided";
+                if (sRequest.Contains("?")) {
+                    string[] parameters = sRequest.Substring(sRequest.IndexOf("?") + 1).Split('&');
+                    IDictionary<string, string> opts = new Dictionary<string, string>();
+                    foreach (var parameter in parameters) {
+                        string[] opt = parameter.Split('=');
+                        Log(parameter);
+                        if (opt.Length == 2) {
+                            opts.Add(opt[0], opt[1]);
+                        } else {
+                            opts.Add(parameter, "");
+                        }
+                    }
+
+                    string pName = "";
+                    if (opts.ContainsKey("idType") && opts["idType"] == "name") {
+                        pName = opts["name"];
+                    } else if (opts.ContainsKey("idType") && opts["idType"] == "pid") {
+                        int pid = Int32.Parse(opts["pid"]);
+                        foreach (var process in Process.GetProcesses()) {
+                            if (process.Id == pid) {
+                                pName = process.ProcessName;
+                            }
+                        }
+                    } else {
+                        pName = VolumeMixerInterFace.GetForegroundProcessName();
+                    }
+
+                    if (opts["operation"] == "setVolume") {
+                        if (float.TryParse(opts["param"], out float vol)) {
+                            output = "Set " + pName + " to " + vol + "%.";
+                            VolumeMixerInterFace.SetApplicationVolume(pName, vol);
+                        } else {
+                            output = "Not a valid volume.";
+                        }
+                    } else if (opts["operation"] == "setMute") {
+                        bool mute = bool.Parse(opts["param"]);
+                        VolumeMixerInterFace.SetApplicationMute(pName, mute);
+                        output = "Set " + pName + " to be " + (mute ? "" : "un") + "muted.";
+                    } else if (opts["operation"] == "toggleMute") {
+                        VolumeMixerInterFace.ToggleApplicationMute(pName);
+                        output = "Toggled " + pName + "'s muted-ness";
+                    } else if (opts["operation"] == "incrementVolume") {
+                        if (float.TryParse(opts["param"], out float volInc)) {
+                            output = (volInc > 0 ? "+" : "") + volInc + "% to " + pName + ".";
+                            VolumeMixerInterFace.IncrementApplicationVolume(pName, volInc);
+                        } else {
+                            output = "Not a valid increment value.";
+                        }
+                    }
+                }
+
+                SendHeader(sHttpVersion, "text/plain", output.Length, " 200 OK", ref socket);
+                SendToBrowser(output, ref socket);
+                socket.Close();
+                continue;
+            }
+        }
+    }
+}
